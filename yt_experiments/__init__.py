2d30a5af5bd322cd91c05b0c3626b12a49fbcf08
chavlin <chris.havlin@gmail.com>
2023-11-17 10:37:59 -0600

    initial project setup

---
+++ yt_experiments/__init__.py
@@ -0,0 +1,3 @@
+from yt_experiments._version import __version__
+
+version = __version__
---
+++ yt_experiments/tests/test_yt_experiments.py
@@ -0,0 +1,5 @@
+import yt_experiments
+
+
+def test_yt_experiments():
+    assert yt_experiments.version is not None
