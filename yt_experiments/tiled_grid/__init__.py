80f1aabff0baae01de9f14173e6f67a60a23fcf5
chavlin <chris.havlin@gmail.com>
2024-10-03 15:21:14 -0500

    simplify, add tests

--- yt_experiments/tiled_grid/__init__.py
+++ yt_experiments/tiled_grid/__init__.py
@@ -1,1 +1,5 @@
-from .tiled_grid import YTOctPyramid, YTPyramid, YTTiledArbitraryGrid  # noqa: F401
+from .tiled_grid import (
+    YTArbitraryGridOctPyramid as YTArbitraryGridOctPyramid,
+    YTArbitraryGridPyramid as YTArbitraryGridPyramid,
+    YTTiledArbitraryGrid as YTTiledArbitraryGrid,
+)
